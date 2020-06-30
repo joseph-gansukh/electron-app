91c6cde8552a2ae28da8c6cf58881c2b2095aa55
joseph-gansukh <g.sukhbat@gmail.com>
2020-06-29 23:06:44 -0500

    prints to pdf

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 350,
+        height: 560,
         webPreferences: {
             nodeIntegration: true
         }
